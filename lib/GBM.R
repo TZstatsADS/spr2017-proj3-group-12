beed481a81b2ed14bce7f4312acef2cdf8f82313
vikasarun <vikas.arun@gmail.com>
2017-03-19 14:33:05 -0400

    Small GBM change

--- lib/GBM.R
+++ lib/GBM.R
@@ -18,7 +18,7 @@ ExploreGBM = function(feature_filename, labels_filename)
   plot(tune_gbm$results$shrinkage[7:9], tune_gbm$results$Accuracy[7:9], type = "b", col = "red", lty = 2, xlab = "shrinkage", ylab = "accuracy", main = "Accuracy vs. Shrinkage", ylim = c(0.65,0.78), pch = 1)
   points(tune_gbm$results$shrinkage[4:6], tune_gbm$results$Accuracy[4:6], type = "b", col = "blue", lty = 2, pch = 2)
   points(tune_gbm$results$shrinkage[1:3], tune_gbm$results$Accuracy[1:3], type = "b", col = "purple", lty = 2, pch = 3)
-  legend("topleft", c("1000", "500", "100"), col = c("red","blue","purple"), pch = c(1,2,3), title = "Num. Stumps")
+  legend("topleft", c("1000", "500s", "100"), col = c("red","blue","purple"), pch = c(1,2,3), title = "Num. Stumps")
   dev.off()
   
 }
@@ -33,7 +33,8 @@ TrainGBM = function(feature_filename,labels_filename, param_trees = 500, full_fe
   fitControl = trainControl(method = "repeatedcv", number = 5, repeats = 1)
   tune_gbm = train(image_features, as.factor(image_labels), method = "gbm", tuneGrid = parameters, trControl = fitControl)
   train_time = (proc.time() - t)[3]
-  cat("Elapsed prediction time for  GBM with ", param_trees, "trees and shrinkage 0.1 is " , train_time, " seconds \n")
+  cat("Elapsed training time for  GBM with ", param_trees, "trees and shrinkage 0.1 is " , train_time, " seconds \n")
+  cat("Validation error for GBM is ", 1 - tune_gbm$results$Accuracy)
   filename = "../output/GBMModifiedFeature.RData"
   if(full_feature == TRUE)
   {
@@ -48,7 +49,7 @@ TestGBM = function(gbm_model, feature_filename, param_trees = 500, full_feature 
 {
   t = proc.time()
   image_features = t(read.csv(feature_filename))
-  gbm_predict = predict(tune_gbm, image_features, n.trees = param_trees)
+  gbm_predict = predict(gbm_model, image_features, n.trees = param_trees)
   test_time = (proc.time() - t)[3]
   cat("Elapsed prediction time for  GBM with ", param_trees, " trees is ", test_time, " seconds \n")
   filename = "../output/GBMModifiedPredictions.csv"
