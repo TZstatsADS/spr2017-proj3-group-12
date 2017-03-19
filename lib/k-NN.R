494c8007d777e7b55eee37dc0f4a9241566b792c
ptfairy <ptfairyxxy@gmail.com>
2017-03-19 17:25:51 -0400

    basic k-NN

--- lib/k-NN.R
+++ lib/k-NN.R
@@ -25,5 +25,5 @@ sift_test <- sift_test[-1]
 sift_train_labels <- sift_train[, 1]
 sift_test_labels <- sift_test[, 1]
 
-sift_test_pred <- knn(train = sift_train, test = sift_test, cl = sift_train_labels, k=3)
+sift_test_pred <- knn(train = sift_train, test = sift_test, cl = sift_train_labels, k=5)
 CrossTable(x = sift_test_labels, y = sift_test_pred, prop.chisq = FALSE )
