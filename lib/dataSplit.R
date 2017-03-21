1c36ee957c0baf85b2ecb83ccd1c94bfaa41ddfb
bz2290 <bz2290@columbia.edu>
2017-03-21 13:06:44 -0400

    update

--- lib/dataSplit.R
+++ lib/dataSplit.R
@@ -2,12 +2,30 @@
 
 dataSplit = function(percentage = 0.25)
 {
-  image_features = read.csv("./data/sift_features.csv")
   source("./lib/feature.R")
+  
+  image_features = read.csv("./data/sift_features.csv")
+  
   #image_features_new = read.csv("../data/sift.feature.New.csv")
   image_features_new = feature.new(image_features)
+  
+  image_features_gray = read.csv("./data/gray.csv")
+  
+  #Remove the first column of observation numbers
+  image_features_gray = image_features_gray[,2:2001]
+  
+  #Remove fetures that have no variation (if any)
+  image_features_gray = image_features_gray[which(apply(image_features_gray,1,sd)!=0),]
+  
+  #Rename the columns
+  colnames(image_features_gray) = colnames(image_features_new)
+  
+  image_features_new_gray = rbind(image_features_new,image_features_gray)
+  
   image_labels = read.csv("./data/labels.csv")
+  
   n = ncol(image_features)
+  
   test_rows = sample(c(1:n), percentage*n, replace = FALSE)
   #train_rows = -test_rows
   write.csv(image_features[,-test_rows], file = "./data/sift_features_train.csv", row.names = FALSE)
@@ -16,6 +34,6 @@ dataSplit = function(percentage = 0.25)
   write.csv(image_labels[test_rows,], file = "./data/labels_test.csv", row.names = FALSE)
   write.csv(image_features_new[,-test_rows], file = "./data/sift_features_new_train.csv", row.names = FALSE)
   write.csv(image_features_new[,test_rows], file = "./data/sift_features_new_test.csv", row.names = FALSE)
-  #write.csv()
-  #write.csv()
+  write.csv(image_features_new_gray[,-test_rows], file = "./data/sift_new_gray_train.csv", row.names = FALSE)
+  write.csv(image_features_new_gray[,test_rows], file = "./data/sift_new_gray_test.csv", row.names = FALSE)
 }
